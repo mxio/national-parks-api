4d21785dbae57ed39b58ea5882e11ea9751a16ca
mxio <maitongxiong@gmail.com>
2019-03-01 22:41:06 -0600

    added array of states functionality

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 'use strict;'
 
 const apiKey = 'ebf8wzTelsRC8L8vTGnyXgnmpWpxEloj8KbJfsMX';
-const searchURL = 'api.nps.gov/api/v1/parks'
+const searchURL = 'api.nps.gov/api/v1/parks';
+
+const states = 
 
 function displayData() {
-
+    //name, description, url
 }
 
 function getData() {
 
-    displayData();
+    const params = {
+        key: apiKey,
+        q: query,
+        stateCode: states,
+        limit: maxResults
+    }
+    displayData(params, );
 }
 
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
+
+        const maxResults = $('#results').val();
+        
+        const searchInputs = $(':text');
+        const states = searchInputs.map(function(input) {
+            return this.value;
+        })
+
         getData();
     })
 }
