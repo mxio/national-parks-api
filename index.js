a0b4be8863303495bd842227275418c55d49c575
mxio <maitongxiong@gmail.com>
2019-03-01 21:05:44 -0600

    added base functions

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 'use strict;'
 
+const apiKey = 'ebf8wzTelsRC8L8vTGnyXgnmpWpxEloj8KbJfsMX';
+const searchURL = 'api.nps.gov/api/v1/parks'
+
+function displayData() {
+
+}
+
+function getData() {
+
+    displayData();
+}
 
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
-        
+        getData();
     })
 }
 
